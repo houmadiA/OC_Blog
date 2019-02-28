4e3b493983c431fa5745273041fd9d6191e055d4
mbenfredj <benfredjmajdi@gmail.com>
2019-02-28 13:25:59 +0100

    Creating postList and postListItem components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oc-blog-project';
+  postConent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent viverra convallis augue, sed commodo arcu ornare quis.';
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: this.postConent,
+      loveIts: 2,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: this.postConent,
+      loveIts: -1,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon troisième post',
+      content: this.postConent,
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
